94e60b8ed7dd1de9703a1a5dcd32ab3af12a5fff
linux_china <linux_china@hotmail.com>
2018-07-19 09:31:32 -0700

    polishing

--- src/main/java/org/mvnsearch/fn/HelloFunction.kt
+++ src/main/java/org/mvnsearch/fn/HelloFunction.kt
@@ -3,7 +3,7 @@ package org.mvnsearch.fn
 class HelloFunction {
 
     fun handleRequest(name: String = "world"): String {
-        return "Good morning, $name!"
+        return "Hello, $name!"
     }
 
 }
